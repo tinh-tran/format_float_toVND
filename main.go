645def2706e4729227de1df359448107ad0b2658
Tính Trần <jackymon41@gmail.com>
2019-09-28 15:11:23 +0700

    fix bugs

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 }
 
 func FormatAmount(m float64) string {
+	m = math.Round(m)
 	if m == 0 {
 		return "-"
 	}
-	m = math.Round(m)
 	n := int64(m)
 	in := strconv.FormatInt(n, 10)
 	out := make([]byte, len(in)+(len(in)-2+int(in[0]/'0'))/3)
